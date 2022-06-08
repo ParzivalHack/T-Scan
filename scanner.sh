04cfe15a389880ba2f818b0b5df00dd4124c0765
Tommaso Bona <piergeolo@gmail.com>
2022-06-08 23:42:27 +0200

    Update scanner.sh

--- scanner.sh
+++ scanner.sh
@@ -32,40 +32,11 @@ ping $host
 elif [ $optnz = "2" ];
 then
 banner
-scan
+bash scan.sh
 elif [ $optnz = "3" ];
 then
 bash vuln.sh
 fi
-}
-scan() {
-  if [[ -z $1 || -z $2 ]]; then
-    echo "Usage: $0 <host> <port, ports, or port-range>"
-    return
-  fi
-
-  local host=$1
-  local ports=()
-  case $2 in
-    *-*)
-      IFS=- read start end <<< "$2"
-      for ((port=start; port <= end; port++)); do
-        ports+=($port)
-      done
-      ;;
-    *,*)
-      IFS=, read -ra ports <<< "$2"
-      ;;
-    *)
-      ports+=($2)
-      ;;
-  esac
-
-  for port in "${ports[@]}"; do
-    alarm 1 "echo >/dev/tcp/$host/$port" &&
-      echo "$green Port $port is open" ||
-      echo "$red Port $port is closed"
-  done
 }
 banner
 menu
