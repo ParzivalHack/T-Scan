33aa941684b97ad237d5eccc011461154a959b38
Tommaso Bona <piergeolo@gmail.com>
2022-06-08 22:30:16 +0200

    Update scanner.sh

--- scanner.sh
+++ scanner.sh
@@ -12,3 +12,59 @@ echo -e "$blue
 ::: ##:::::::::::::. ######::. ######:: ##:::: ##: ##::. ##:
 :::..:::::::::::::::......::::......:::..:::::..::..::::..::
 "
+}
+banner
+menu(){
+echo -e "$blue             ＞＞＞＞>>>＞$blue[Menu]$blue ＜<<<＜＜＜＜$blue"
+echo " "
+echo " "
+echo -e "$blue                        ➡$blue [\e[34m1\e[34m]$blue Ping "
+echo -e "$blue                        ➡$blue [\e[34m2\e[34m]$blue Scan "
+echo -e "$blue                        ➡$blue [\e[34m3\e[34m]$blue Check vulnerabilities "
+echo -ne "\e[34m#Select Option\e[34m: "
+read optnz
+if [ $optnz = "1" ];
+then
+banner
+sleep 1
+echo -ne "\e[34m#Select host to ping (website.com)\e[34m: "
+read host
+ping $host.com
+done
+elif [ $optnz = "2" ];
+then
+banner
+scan
+elif [ $optnz = "3" ];
+then
+bash vuln.sh
+}
+scan() {
+  if [[ -z $1 || -z $2 ]]; then
+    echo "Usage: $0 <host> <port, ports, or port-range>"
+    return
+  fi
+
+  local host=$1
+  local ports=()
+  case $2 in
+    *-*)
+      IFS=- read start end <<< "$2"
+      for ((port=start; port <= end; port++)); do
+        ports+=($port)
+      done
+      ;;
+    *,*)
+      IFS=, read -ra ports <<< "$2"
+      ;;
+    *)
+      ports+=($2)
+      ;;
+  esac
+
+  for port in "${ports[@]}"; do
+    alarm 1 "echo >/dev/tcp/$host/$port" &&
+      echo "$green Port $port is open" ||
+      echo "$red Port $port is closed"
+  done
+}
